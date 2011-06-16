b0e1fc2b37b405b30c2655f3efb8687aab85075f
Zdenek Styblik <stybla@turnovfree.net>
2011-06-16 14:18:08 +0200

    MOD added GNU GPL v2 header Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>

--- includes/functions.tcl
+++ includes/functions.tcl
@@ -1,12 +1,21 @@
 #
-# functions for TkGUI.tck
+# Desc: functions for TkGUI.tck
+# Copyright (C) 2006 Davide Zito
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 #
-# Slack-Kickstart-10.2
-# Comments to: vonatar@slack-kickstart.org
-#
-# Last Revision 25/02/2006 vonatar
-#
-
 #-------------------------------------
 # ENABLES/DISABLES INET STUFF
 #-------------------------------------
