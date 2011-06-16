f1de635bee5e74f0f51fb8aef71e4e320ad3805f
Zdenek Styblik <stybla@turnovfree.net>
2011-06-16 13:49:15 +0200

    MOD added GNU GPL v2 header Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>

--- scripts/Tag2List.sh
+++ scripts/Tag2List.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
-#
+# Desc: Converts tagfiles to taglist
+# Copyright (C) 2006 Slack-Kickstart
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
 # Tag2List.sh
 #
-# Created: 25/02/2006
-# Last Revision: -
-#
-# Converts tagfiles to taglist
-#
-
 help()
 {
 	printf "#\n# Converts Slackware tagfiles in taglist format:\n"
@@ -15,7 +25,7 @@ help()
 	printf "# Example: to convert mini-tag Tagfile\n"
 	printf "# %s tagfiles/mini-tag \n#\n" ${0}
 	return 0
-}
+} # help()
 
 FILEIN=${1:-'None'}
 
