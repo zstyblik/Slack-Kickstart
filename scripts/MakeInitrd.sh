732039ebf2b0bd1c9db5b64d2441891eaec51f48
Zdenek Styblik <stybla@turnovfree.net>
2011-06-16 13:46:48 +0200

    MOD added GNU GPL v2 header Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>

--- scripts/MakeInitrd.sh
+++ scripts/MakeInitrd.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
+# Desc: Creates an image configured by parameters - config, kernel, template
 #
+# Copyright (C) 2006 Vonatar <vonatar@slack-kickstart.org>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+# 
 #################
 # MakeInitrd.sh #
 #################
-#
-# Slack-Kickstart
-#
-# Last Revision: 24/07/2006 vonatar
-#
-# comments to: vonatar@slack-kickstart.org
-# http://www.slack-kickstart.org
-#
 set -e
 set -u
 
