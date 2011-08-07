57731818e7f27751247660827598e4f2c37d1467
Zdenek Styblik <stybla@turnovfree.net>
2011-08-07 14:38:43 +0200

    ADD post-install directory Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>

---
+++ post-install/ssh.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# SSH post-inst
+printf "Altering '/etc/ssh/sshd_config'.\n"
+sed -r -e '/^[#]?PasswordAuthentication/c \PasswordAuthentication no' \
+	-e '/^[#]?Protocol/c \Protocol 2' /etc/ssh/sshd_config \
+	-e '/^[#]?PubkeyAuthentication/c \PubkeyAuthentication yes' \
+	> /etc/ssh/sshd_config.new
+mv /etc/ssh/sshd_config.new /etc/ssh/sshd_config
