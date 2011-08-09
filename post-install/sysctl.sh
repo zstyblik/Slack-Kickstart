41cf95556d8dbddf71ff74500e40db05870fecfd
Zdenek Styblik <stybla@turnovfree.net>
2011-08-09 06:21:05 +0200

    MOD sysctl.conf ~ error handling Signed-off-by: Zdenek Styblik <stybla@turnovfree.net>
    This patch should help to handle missing /etc/sysctl.conf file
    which doesn't have to be present prior running this script.

--- post-install/sysctl.sh
+++ post-install/sysctl.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 SYSCTLFILE="/etc/sysctl.conf"
 printf "Post-install ~ '/etc/sysctl.conf'\n"
-cp ${SYSCTLFILE} ${SYSCTLFILE}.org
+cp ${SYSCTLFILE} ${SYSCTLFILE}.org 2>/dev/null
 cat >> ${SYSCTLFILE}.new <<EOF
 kernel.panic = 60
 EOF
 
-cat ${SYSCTLFILE}.new ${SYSCTLFILE}.org | sort | uniq > ${SYSCTLFILE}
+cat ${SYSCTLFILE}.new ${SYSCTLFILE}.org 2>/dev/null | sort | \
+	uniq > ${SYSCTLFILE}
